a5fbff9db88d28c8cb385273b44c2c8c945cff9e
Bobby Kuepper <robertkuepper@gmail.com>
2023-10-31 13:53:19 -0500

    test

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+// index.js
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph) {
+  cats.push(Ralph);
+}
+
+function destructivelyPrependCat(Bob) {
+  cats.unshift(Bob);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(Broom) {
+  const newCats = [...cats, Broom];
+  return newCats;
+}
+
+function prependCat(Arnold) {
+  const newCats = [Arnold, ...cats];
+  return newCats;
+}
+
+function removeLastCat() {
+  const newCats = cats.slice(0, -1);
+  return newCats;
+}
+
+function removeFirstCat() {
+  const newCats = cats.slice(1);
+  return newCats;
+}
